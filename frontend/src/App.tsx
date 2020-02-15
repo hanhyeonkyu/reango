e829150efb97d4fafee9e33e946bcd9e3e27eb3e
hanhyeonkyu <busgod1212@gmail.com>
2020-02-15 21:51:33 +0900

    apply react-router-dom

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RoutesConfig from './layout/RoutesConfig'
+
+function App() {
+  return (
+    <div className="App">
+      <RoutesConfig/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/container/landing/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="Home">
+      React app
+    </div>
+  );
+}
+
+export default Home;
